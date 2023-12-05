58456e32c732d6a7cfda9ee507a15b535a7c0319
Atilla <atillaagungorr@gmail.com>
2023-12-05 21:44:58 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Core.DataAccess.Paging;
+using Core.DataAccess;
+using Entities.Concretes;
+using Octopus.Client.Repositories.Async;
+using DataAccess.Abstracts;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public async Task Add(Product product)
+        {
+            await _productDal.AddAsync(product);
+        }
+
+        public async Task<Paginate<Product>> GetListAsync()
+        {
+            var result = await _productDal.GetListAsync();
+            return result;
+        }
+    }
+}
